54a7426355fab1721614a7ea1f498cd93e962c1d
kotelevsky <Pavel.Kotelevsky@Dell.com>
2019-10-26 01:17:26 +0300

    Init

---
+++ rest-api/src/main/java/ru/advpad/VoiceSdkApplication.java
@@ -0,0 +1,13 @@
+package ru.advpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceSdkApplication.class, args);
+	}
+
+}
